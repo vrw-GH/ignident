7acf600820348320aa6010355a26f9ea2c6fc40e
Victor Wright <victor.wright@outlook.de>
2025-04-04 13:53:17 +0200

    folder update (staging.xxx removed)

--- builds/_staging.(250401-1613)/wp-content/plugins/wp-seopress/inc/admin/blocks/insights.php
+++
@@ -1,4 +0,0 @@
-<?php
-    defined('ABSPATH') or exit('Please don&rsquo;t call the plugin directly. Thanks :)');
-
-    do_action('seopress_dashboard_insights', $current_tab);
--- builds/_staging.(250401-1613)/wp-content/plugins/wp-seopress/public/editor/blocks/faq-v2/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2a3839ca5e90e9b76203');
